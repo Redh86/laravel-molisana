7ce77f84515084b4d35682b4b4e675c656e0f1c9
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-05-06 17:33:32 +0200

    contenuti ok

--- MyProject/resources/views/prodotti.blade.php
+++ MyProject/resources/views/prodotti.blade.php
@@ -1,12 +1,19 @@
 @extends('layout.molisana')
 
+@dump($paste)
+
 @section('titolo_pagina', 'Prodotti')
 
 @section('content')
     <div class="wrap-cards">
+        @foreach ($paste as $pasta)
+            <div class="card">
+                <img src="{{ $pasta['src'] }}" alt="">
+                <div class="overlay">
+                    <h1>{{ $pasta['titolo'] }}</h1>
+                    <h1>_</h1>
+            </div>  
+        @endforeach
 
-        <div class="card">
-
-        </div>
     </div>
 @endsection
